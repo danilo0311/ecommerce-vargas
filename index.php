79e797d2d4356084d0f008e209d66f3a5261e842
Danilo <danilomurillo0311@gmail.com>
2021-10-01 21:10:57 +0200

    root

--- index.php
+++ index.php
@@ -1,75 +1,34 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Multiservicios vargas & vegas</title>
-    <link href="https://fonts.googleapis.com/css2?family=Nixie+One&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="main_files/my_stylesheet/main.css">
-  </head>
-  <body>
-    <div class="header-container">
-      <nav>
-
-      </nav>
-    </div>
-    <div class="main-container">
-      <div class="items">
-        <section class="one">
-            <div class="bloc-item" id="B1">
-              <img src="" alt="">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="main/stylesheet/main.css">
+    <!-- CSS -->
+    <!-- FONTS -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"> 
+    <!-- FONTS -->
+    <title>Ecommerce vargas&vegas</title>
+</head>
+<body> 
+    <div class="container">
+        <div class="header">
+            <div class="header-buttons-wrapper">
+                <button type="button" class="header-buttons" id="home-button">Home</button>
+                <button type="button" class="header-buttons" id="category-button">Categorias</button>
+                <button type="button" class="header-buttons" id="contact-button">Contacto</button>
+                <button type="button" class="header-buttons" id="service-button">Servicios</button>
+                <button type="button" class="header-buttons" id="our-work-button">Nuestro trabajo</button>
+                <button type="button" class="header-buttons" id="estimate-button">Crear presupuesto</button>
+                </div>
+            <div id="company-logo-wrapper">
+                <img src="main/images/vv-logo.png" id="header-company-logo">
+                </div>
             </div>
-            <div class="bloc-item" id="B2">
-              <img src="" alt="">
-            </div>
-            <div class="bloc-item" id="B3">
-              <img src="" alt="">
-            </div>
-            <div class="bloc-item" id="B4">
-              <img src="" alt="">
-            </div>
-          </section>
-        <section class="two">
-            <div class="bloc-item" id="B5">
-              <img src="" alt="">
-            </div>
-            <div class="bloc-item" id="B6">
-              <img src="" alt="">
-            </div>
-            <div class="bloc-item" id="B7">
-              <img src="" alt="">
-            </div>
-            <div class="bloc-item" id="B8">
-              <img src="" alt="">
-            </div>
-          </section>
-      </div>
-    </div>
-    <div class="landing-page">
-      <div class="landing-container">
-        <div class="landing-body">
-          <nav id="landing-nav">
-            <div class="nav-wrapper">
-              <div class="button-wrapper">
-                <button type="button" class="landing-buttons" id="home-button">Home</button>
-              </div>
-              <div class="button-wrapper">
-                <button type="button" class="landing-buttons" id="category-button">categorias</button>
-              </div>
-              <div class="button-wrapper">
-                <button type="button" class="landing-buttons" id="about-us-button">Sobre nosotros</button>
-              </div>
-              <div class="button-wrapper">
-                <button type="button" class="landing-buttons" id="contact-button">Contacto</button>
-              </div>
-              <div class="button-wrapper">
-                <button type="button" class="landing-buttons" id="top-selling-button">Más vendidos</button>
-              </div>
-              <div class="button-wrapper">
-                <button type="button" class="landing-buttons" id="coming-soon-button">Próximamente</button
-              </div>
-            </div>
-          </nav>
-      </div>
-    </div>
-  </body>
+        <div class="body-wrapper"></div>
+        <div class="footer"></div>
+        </div>
+</body>
 </html>
