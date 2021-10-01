87107827a086f1e79124a15d1ab5db4828b94b1f
Danilo <danilomurillo0311@gmail.com>
2021-10-01 21:10:17 +0200

    created the main root files, header and buttons

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Multiservicios vargas & vegas</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nixie+One&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="main_files/my_stylesheet/main.css">
+  </head>
+  <body>
+    <div class="header-container">
+      <nav>
+
+      </nav>
+    </div>
+    <div class="main-container">
+      <div class="items">
+        <section class="one">
+            <div class="bloc-item" id="B1">
+              <img src="" alt="">
+            </div>
+            <div class="bloc-item" id="B2">
+              <img src="" alt="">
+            </div>
+            <div class="bloc-item" id="B3">
+              <img src="" alt="">
+            </div>
+            <div class="bloc-item" id="B4">
+              <img src="" alt="">
+            </div>
+          </section>
+        <section class="two">
+            <div class="bloc-item" id="B5">
+              <img src="" alt="">
+            </div>
+            <div class="bloc-item" id="B6">
+              <img src="" alt="">
+            </div>
+            <div class="bloc-item" id="B7">
+              <img src="" alt="">
+            </div>
+            <div class="bloc-item" id="B8">
+              <img src="" alt="">
+            </div>
+          </section>
+      </div>
+    </div>
+    <div class="landing-page">
+      <div class="landing-container">
+        <div class="landing-body">
+          <nav id="landing-nav">
+            <div class="nav-wrapper">
+              <div class="button-wrapper">
+                <button type="button" class="landing-buttons" id="home-button">Home</button>
+              </div>
+              <div class="button-wrapper">
+                <button type="button" class="landing-buttons" id="category-button">categorias</button>
+              </div>
+              <div class="button-wrapper">
+                <button type="button" class="landing-buttons" id="about-us-button">Sobre nosotros</button>
+              </div>
+              <div class="button-wrapper">
+                <button type="button" class="landing-buttons" id="contact-button">Contacto</button>
+              </div>
+              <div class="button-wrapper">
+                <button type="button" class="landing-buttons" id="top-selling-button">Más vendidos</button>
+              </div>
+              <div class="button-wrapper">
+                <button type="button" class="landing-buttons" id="coming-soon-button">Próximamente</button
+              </div>
+            </div>
+          </nav>
+      </div>
+    </div>
+  </body>
+</html>
