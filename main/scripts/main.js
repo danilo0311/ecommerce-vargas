82607661692d8e9123198e3ac36cf11fda6fc1db
Danilo <danilomurillo0311@gmail.com>
2021-10-15 10:25:09 +0200

    root

---
+++ main/scripts/main.js
@@ -0,0 +1,31 @@
+let exitLogin;
+
+exitLogin = () => {
+    document.getElementsByClassName('contact-wrapper')[0].style.display="none";   
+}
+
+let notifyProgress;
+
+notifyProgress = () => {
+    let progresoI = "PROGRESO: VENTANA DE CONTACTO AÑADIDA";
+    let progresoII = "LANDING PAGE: NUEVA SECCIÓN: PRODUCTOS MAS COTIZADOS";
+    alert(progresoI);
+    alert(progresoII);
+}
+
+let categoryButtonHandler, displayCategory, hideCategory;
+
+categoryButtonHandler = () => {
+    document.getElementById("category-button").addEventListener('mouseout', hideCategory);
+    document.getElementById("category-button").addEventListener('mouseover', displayCategory);
+    document.getElementsByClassName("category-wrapper")[0].addEventListener('mouseover', displayCategory);
+    document.getElementsByClassName("category-wrapper")[0].addEventListener('mouseout', hideCategory);
+  }
+  
+  displayCategory = () => {
+    document.getElementsByClassName("category-wrapper")[0].style.height="321px";
+  }
+  
+  hideCategory = () => {
+    document.getElementsByClassName("category-wrapper")[0].style.height="0px";
+  }
