29fd5bdc5f7679be6d8fc754de0fe6c64796878b
Danilo <danilomurillo0311@gmail.com>
2021-10-15 10:23:36 +0200

    included categoryButtonHandler() to be loaded when the page first loads

--- main/scripts/xhr.js
+++ main/scripts/xhr.js
@@ -1,17 +1,46 @@
 import {landingSlider} from './slider.js';
 
+let url = null;
 const XHR = new XMLHttpRequest();
+let status = XHR.status;
+
+window.loadLandingPage = () => {
+    XHR.open('GET','landing-page.html');
+    XHR.onreadystatechange = () => {
+        if (XHR.readyState == XMLHttpRequest.DONE) {
+           if (status === 0 || (status >= 200 && status < 400)) {
+               document.getElementsByClassName('body-wrapper')[0].innerHTML=XHR.responseText;
+               landingSlider();
+               setUrl();
+               categoryButtonHandler();
+           }
+        }
+    }
+    XHR.send();
+}
 
 window.loadFile = () => {
-    XHR.open('GET', 'landing-page.html');
+    XHR.open('GET', setUrl());
     XHR.onreadystatechange = () => {
         if (XHR.readyState === XMLHttpRequest.DONE) {
-            let status = XHR.status;
             if (status === 0 || (status >= 200 && status < 400)) {
-                document.getElementsByClassName('body-wrapper')[0].innerHTML=XHR.responseText;
-                landingSlider();
+                console.log(setUrl());
+                switch (setUrl()) {
+                    case 'contact.html':
+                    document.getElementsByClassName('contact-wrapper')[0].style.display="flex";
+                    document.getElementsByClassName('contact-wrapper')[0].innerHTML=XHR.responseText;
+                        break;
+                
+                    default:
+                        break;
+                }
             }
         }
     }
     XHR.send();
+}
+
+window.setUrl =() => {
+    document.getElementById('contact-button').onclick = () => {url = "contact.html", loadFile()}
+    return url;
 }
